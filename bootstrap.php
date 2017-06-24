fc7609c07aa9f839af1f214d35fa2376cb0fd0f9
Ronnie B <trafficinc@users.noreply.github.com>
2017-06-24 12:33:45 -0400

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -1,8 +1,5 @@
 <?php
 
-
-//require_once 'model.php';
-
 use Illuminate\Database\Capsule\Manager as Capsule;
 
 define('BASE_PATH', __DIR__);
@@ -20,17 +17,3 @@ $capsule = new Capsule;
 $capsule->addConnection(require BASE_PATH . '/app/Config/database.php');
 $capsule->setAsGlobal();
 $capsule->bootEloquent();
-
-// $loader = new Twig_Loader_Filesystem(BASE_PATH . '/app/Views');
-// // BASE_PATH . '/app/Views/cache' << -- caching
-// $twig = new Twig_Environment($loader, array(
-//     'cache' => FALSE,
-// ));
-
-//echo $twig->render('base.html');
-
-//echo $twig->render('home.html');
-
-//echo $twig->render('index.html', array('name' => 'Fabien'));
-
-//require_once BASE_PATH . '/app/Config/routes.php';
